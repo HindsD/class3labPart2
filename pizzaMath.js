780757e12c7325d9e79741597ebb81f611039516
Danny Hinds <dhinds1@my.wctc.edu>
2021-01-27 16:26:28 -0600

    switching to jquery

--- pizzaMath.js
+++ pizzaMath.js
@@ -1,9 +1,36 @@
+//page load glob of code
+
+
+$(document).ready(
+    function(){
+        // find the button with jquery
+
+        // assign function to run when the button is clicked
+
+        $("button").click(pizzaMath);
+    }
+);
+
 function pizzaMath(){
-    let totalPizza = parseFloat( prompt("How many Pizza's will you order"));
-    let totalToppings = parseFloat(prompt("What is the amount of total toppings?"));
-    let totalPeople = parseFloat(prompt("How many of you are there total?"));
+    //let totalPizza = parseFloat( prompt("How many Pizza's will you order"));
+
+    let totalPizza = parseFloat( $("input#totalPizza").val() );
+
+    //let totalToppings = parseFloat(prompt("What is the amount of total toppings?"));
+
+    let totalToppings = parseFloat( $("input#totalToppings").val() );
+
+    //let totalPeople = parseFloat(prompt("How many of you are there total?"));
+
+    let totalPeople = parseFloat( $("input#totalPeople").val() );
+
     let totalCost = (totalPizza * 15) + (totalToppings * 1.25);
+
     let costPerPerson = totalCost / totalPeople;
+
     let displayCPP = costPerPerson.toFixed(2);
-    alert(`The cost will be $${displayCPP} per person.`)
+
+    //alert(`The cost will be $${displayCPP} per person.`)
+    $("#displayCPP").text(displayCPP);
+
 }
